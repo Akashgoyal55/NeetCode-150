de90777d75584fe3687217744bc029ef3ff6fb10
Akashgoyal55 <agoyal1@sfsu.edu>
2024-12-20 21:21:09 -0800

    test

--- src/LinkedList/ReverseLinkedList.java
+++ src/LinkedList/ReverseLinkedList.java
@@ -1,4 +1,20 @@
 package LinkedList;
 
 public class ReverseLinkedList {
+    public static void main(String[] args) {
+
+    }
+    //to reverse a linked list, we need three pointers
+    static listNode(ListNode head){
+        listNode before = null; //take pointers before, curr, after, then do operations to reverse the linked list
+        listNode curr = head;
+        listNode after;
+        while(curr!=null){
+            after = curr.next;
+            curr.next = before;
+            before = curr;
+            curr = after;
+        }
+        return before;
+    }
 }
